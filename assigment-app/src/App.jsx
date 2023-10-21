7089fb25b74c9b8d1fbc44b56f31f856e405edcd
RidhoPutraBramantyo <126537811+RidhoPutraBramantyo@users.noreply.github.com>
2023-10-21 18:07:22 +0800

    Start Buat Aplikasi Baru Untuk Tugas Eduwork

---
+++ assigment-app/src/App.jsx
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import FormElement from "./components/Form";
+function App() {
+  return (
+    <div className="App">
+      <FormElement />
+    </div>
+  );
+}
+
+export default App;
