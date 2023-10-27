f695d6515282a9a6cfa4b1d5902744f497773804
RidhoPutraBramantyo <126537811+RidhoPutraBramantyo@users.noreply.github.com>
2023-10-27 19:54:56 +0800

    Major updates

--- assigment-app/src/Pages/HomePage.jsx
+++ assigment-app/src/Pages/HomePage.jsx
@@ -1,28 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import video from "../assets/Aq3ra.mp4";
 import "../css/HomePage.css"; // Impor file CSS Anda
 
 const HomePage = () => {
   return (
     <div className="home-page">
-      <h1 className="welcome-heading">Welcome to My App</h1>
-      <p className="app-description">Here you can check my Assignment</p>
-      <div className="button-container">
-        <Link to="/registration">
-          <button className="registration-button">Registration Page</button>
-        </Link>
-        <Link to="/signin">
-          <button className="signin-button">Sign In</button>
-        </Link>{" "}
-        <Link to="/signup">
-          <button className="signup-button">Sign Up</button>
-        </Link>
-        <Link to="/withFormik">
-          <button className="with-formik-button">withFormik</button>
-        </Link>
-        <Link to="/news">
-          <button className="news-portal-button">News App</button>
-        </Link>
+      <video
+        autoPlay
+        muted
+        loop
+        id="background-video"
+        className="video-container video-container-16-9"
+      >
+        <source src={video} type="video/mp4" />
+      </video>
+
+      <div className="content">
+        <h1 className="welcome-heading">Welcome to My App</h1>
+        <p className="app-description">Here you can check my Assignment</p>
+        <div className="button-container">
+          <Link to="/registration">
+            <button className="registration-button">Registration Page</button>
+          </Link>
+          <Link to="/signin">
+            <button className="signin-button">Sign In</button>
+          </Link>
+          <Link to="/signup">
+            <button className="signup-button">Sign Up</button>
+          </Link>
+          <Link to="/withFormik">
+            <button className="with-formik-button">withFormik</button>
+          </Link>
+          <Link to="/news">
+            <button className="news-portal-button">News App</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
