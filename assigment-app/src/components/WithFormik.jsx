31a70bf0ac99db7c400cd8b0320baf0e7580e2a9
RidhoPutraBramantyo <126537811+RidhoPutraBramantyo@users.noreply.github.com>
2023-10-28 09:01:58 +0800

    CSS update "WithFormik.css"

--- assigment-app/src/components/WithFormik.jsx
+++ assigment-app/src/components/WithFormik.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import "../css/WithFormik.css";
 
 const WithFormik = () => {
   return (
@@ -21,20 +22,29 @@ const WithFormik = () => {
           alamat: Yup.string().required("Alamat wajib diisi"),
         })}
         onSubmit={(values) => {
-          // Di sini Anda dapat mengirimkan data pendaftaran ke server atau menampilkan hasil ke komponen lain.
-          alert("Registrasi berhasil: " + JSON.stringify(values));
+          // Here you can send registration data to the server or display the result in another component.
+          alert("Registration successful: " + JSON.stringify(values));
         }}
       >
-        <Form>
-          <div>
-            <label>Nama:</label>
-            <Field type="text" name="nama" />
+        <Form className="registration-form">
+          <div className="field-group">
+            <label htmlFor="nama" className="field-label">
+              Nama:
+            </label>
+            <Field type="text" id="nama" name="nama" className="field-input" />
             <ErrorMessage name="nama" component="div" className="error" />
           </div>
 
-          <div>
-            <label>Jurusan:</label>
-            <Field as="select" name="jurusan">
+          <div className="field-group">
+            <label htmlFor="jurusan" className="field-label">
+              Jurusan:
+            </label>
+            <Field
+              as="select"
+              id="jurusan"
+              name="jurusan"
+              className="field-input"
+            >
               <option value="">Pilih Jurusan</option>
               <option value="Teknik Informatika">Teknik Informatika</option>
               <option value="Sistem Informasi">Sistem Informasi</option>
@@ -45,28 +55,55 @@ const WithFormik = () => {
             <ErrorMessage name="jurusan" component="div" className="error" />
           </div>
 
-          <div>
-            <label>Jenis Kelamin:</label>
-            <Field type="radio" name="gender" value="Laki-Laki" /> Laki-laki
-            <Field type="radio" name="gender" value="Perempuan" /> Perempuan
+          <div className="field-group">
+            <label className="field-label">Jenis Kelamin:</label>
+            <div>
+              <label htmlFor="gender-laki" className="field-radio-label">
+                <Field
+                  type="radio"
+                  id="gender-laki"
+                  name="gender"
+                  value="Laki-Laki"
+                />{" "}
+                Laki-laki
+              </label>
+              <label htmlFor="gender-perempuan" className="field-radio-label">
+                <Field
+                  type="radio"
+                  id="gender-perempuan"
+                  name="gender"
+                  value="Perempuan"
+                />{" "}
+                Perempuan
+              </label>
+            </div>
             <ErrorMessage name="gender" component="div" className="error" />
           </div>
 
-          <div>
-            <label>Alamat:</label>
-            <Field as="textarea" name="alamat" />
+          <div className="field-group">
+            <label htmlFor="alamat" className="field-label">
+              Alamat:
+            </label>
+            <Field
+              as="textarea"
+              id="alamat"
+              name="alamat"
+              className="field-textarea"
+            />
             <ErrorMessage name="alamat" component="div" className="error" />
           </div>
 
-          <div>
-            <label>
+          <div className="field-group">
+            <label className="field-checkbox-label">
               Member:
               <Field type="checkbox" name="member" />
             </label>
           </div>
 
-          <div>
-            <button type="submit">Submit</button>
+          <div className="field-group">
+            <button type="submit" className="submit-button">
+              Submit
+            </button>
           </div>
         </Form>
       </Formik>
