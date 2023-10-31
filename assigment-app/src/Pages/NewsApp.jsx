f74a2eff4aff8f27aa124d614c448b1ece903689
RidhoPutraBramantyo <126537811+RidhoPutraBramantyo@users.noreply.github.com>
2023-10-31 10:00:28 +0800

    update NewsApp

--- assigment-app/src/Pages/NewsApp.jsx
+++ assigment-app/src/Pages/NewsApp.jsx
@@ -1,47 +1,119 @@
-import React, { useState, useEffect } from "react";
-import Header from "../components/News/Header";
-import LatestNews from "../components/News/LatestNews";
-import CategoryNews from "../components/News/CategoryNews";
-import NewsDetail from "../components/News/NewsDetail";
-import NewsPortal from "../components/News/NewsPortal";
+import React, { Component } from "react";
+import axios from "axios";
 import "../css/NewsApp.css"; // Import file CSS Anda
 
-function NewsApp() {
-  const [news, setNews] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState("all");
-  const [selectedArticle, setSelectedArticle] = useState(null);
+class NewsApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: [],
+      selectedCategory: "all",
+      selectedArticle: null,
+      searchTerm: "",
+    };
+  }
 
-  useEffect(() => {
-    // Gantilah 'URL_API_ANDA' dengan URL API yang sesuai
-    fetch("URL_API_ANDA")
-      .then((response) => response.json())
-      .then((data) => {
-        setNews(data);
+  componentDidMount() {
+    this.fetchNewsData();
+  }
+
+  fetchNewsData() {
+    const API_KEY = "231cf509958443008bff398d363fd7b3"; // Gantilah dengan kunci API Anda
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        this.setState({ news: response.data.articles });
+        console.log(response); // Untuk pemecahan masalah, tampilkan respons API di konsol.
       })
       .catch((error) => {
         console.error("Error fetching data: ", error);
       });
-  }, []);
+  }
 
-  return (
-    <div className="news-app">
-      <Header />
-      <LatestNews newsData={news} />
-      <CategoryNews
-        newsData={news}
-        selectedCategory={selectedCategory}
-        onSelectCategory={(category) => setSelectedCategory(category)}
-      />
-      {selectedArticle ? (
-        <NewsDetail article={selectedArticle} />
-      ) : (
-        <NewsPortal
-          newsData={news}
-          onSelectArticle={(article) => setSelectedArticle(article)}
-        />
-      )}
-    </div>
-  );
+  handleCategoryChange = (category) => {
+    this.setState({ selectedCategory: category });
+  };
+
+  handleArticleSelect = (article) => {
+    this.setState({ selectedArticle: article });
+  };
+
+  handleSearchChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  handleSearchSubmit = () => {
+    this.fetchNewsData(); // Panggil fungsi pencarian saat tombol "Cari" ditekan
+  };
+
+  render() {
+    const { news, selectedCategory, selectedArticle, searchTerm } = this.state;
+
+    const filteredNews =
+      selectedCategory === "all"
+        ? news
+        : news.filter((article) => article.category === selectedCategory);
+
+    const filteredNewsWithSearch = searchTerm
+      ? filteredNews.filter((article) =>
+          article.title.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      : filteredNews;
+
+    const limitedNews = filteredNewsWithSearch.slice(0, 20);
+
+    return (
+      <div className="news-app">
+        <h1>News Portal</h1>
+        <div className="category-selector">
+          <span>Category:</span>
+          <select
+            value={selectedCategory}
+            onChange={(e) => this.handleCategoryChange(e.target.value)}
+          >
+            <option value="all">All</option>
+            <option value="business">Business</option>
+            <option value="technology">Technology</option>
+            <option value="health">Health</option>
+          </select>
+        </div>
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Search News"
+            value={searchTerm}
+            onChange={this.handleSearchChange}
+          />
+          <button onClick={this.handleSearchSubmit}>Cari</button>
+        </div>
+        <div className="news-container">
+          {limitedNews.length > 0 ? (
+            limitedNews.map((article, index) => (
+              <div
+                key={index}
+                className="news-card"
+                onClick={() => this.handleArticleSelect(article)}
+              >
+                <img src={article.urlToImage} alt={article.title} />
+                <h3>{article.title}</h3>
+                <p>{article.description}</p>
+              </div>
+            ))
+          ) : (
+            <div className="no-news-message">No news available.</div>
+          )}
+        </div>
+        {selectedArticle && (
+          <div className="news-detail">
+            <h2>{selectedArticle.title}</h2>
+            <p>{selectedArticle.description}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default NewsApp;
--- assigment-app/src/components/FormElement.jsx
+++ assigment-app/src/components/FormElement.jsx
@@ -1,147 +1,111 @@
-import React from "react";
-import "../css/Form.css"; // Impor file CSS Anda
+import React, { Component } from "react";
+import axios from "axios";
+import "./NewsApp.css"; // Import file CSS Anda
 
-export default class FormElement extends React.Component {
-  state = {
-    nama: "",
-    jurusan: "",
-    gender: "",
-    alamat: "",
-    member: false,
+class NewsApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: [],
+      selectedCategory: "all",
+      selectedArticle: null,
+      searchTerm: "",
+    };
+  }
+
+  componentDidMount() {
+    this.fetchNewsData();
+  }
+
+  fetchNewsData() {
+    const API_KEY = "231cf509958443008bff398d363fd7b3"; // Gantilah dengan kunci API Anda
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        this.setState({ news: response.data.articles });
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }
+
+  handleCategoryChange = (category) => {
+    this.setState({ selectedCategory: category });
   };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    alert(
-      `Nama: ${this.state.nama} 
-      Jurusan: ${this.state.jurusan} 
-      Gender: ${this.state.gender} 
-      Alamat: ${this.state.alamat} 
-      Member: ${this.state.member ? "YES" : "NO"}`
-    );
-    this.setState({
-      nama: "",
-      jurusan: "",
-      gender: "",
-      alamat: "",
-      member: false,
-    });
+  handleArticleSelect = (article) => {
+    this.setState({ selectedArticle: article });
+  };
+
+  handleSearchChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
   };
 
   render() {
+    const { news, selectedCategory, selectedArticle, searchTerm } = this.state;
+
+    // Filter berita berdasarkan kategori yang dipilih
+    const filteredNews =
+      selectedCategory === "all"
+        ? news
+        : news.filter((article) => article.category === selectedCategory);
+
+    // Filter berita berdasarkan kata kunci pencarian
+    const filteredNewsWithSearch = searchTerm
+      ? filteredNews.filter((article) =>
+          article.title.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      : filteredNews;
+
+    // Batasi konten hingga 20 kartu berita
+    const limitedNews = filteredNewsWithSearch.slice(0, 20);
+
     return (
-      <div className="registration-form-container">
-        <form className="form" onSubmit={this.handleSubmit}>
-          <label>
-            Nama:{" "}
-            <input
-              type="text"
-              name="nama"
-              onChange={(e) =>
-                this.setState({ nama: e.target.value }, () =>
-                  console.log(this.state)
-                )
-              }
-              defaultValue={this.props.nama}
-            />
-          </label>
-          <br />
-          <label>
-            Jurusan:{" "}
-            <select
-              name="jurusan"
-              onChange={(e) =>
-                this.setState({ jurusan: e.target.value }, () =>
-                  console.log(this.state)
-                )
-              }
-            >
-              <option value="">Pilih Jurusan</option>
-              <option value="Teknik Informatika">Teknik Informatika</option>
-              <option value="Sistem Informasi">Sistem Informasi</option>
-              <option value="Desain Komunikasi Visual">
-                Desain Komunikasi Visual
-              </option>
-            </select>
-          </label>
-          <br />
-          <label>
-            Jenis Kelamin:
-            <input
-              type="radio"
-              value="Laki-Laki"
-              name="gender"
-              onChange={(e) =>
-                this.setState({ gender: e.target.value }, () =>
-                  console.log(this.state)
-                )
-              }
-            />
-            Laki-laki
-            <input
-              type="radio"
-              value="Perempuan"
-              name="gender"
-              onChange={(e) =>
-                this.setState({ gender: e.target.value }, () =>
-                  console.log(this.state)
-                )
-              }
-            />
-            Perempuan{" "}
-            <input
-              type="radio"
-              value="it's better not to tell"
-              name="gender"
-              onChange={(e) =>
-                this.setState({ gender: e.target.value }, () =>
-                  console.log(this.state)
-                )
-              }
-            />
-            it's better not to tell
-          </label>
-          <br />
-          <label
-            style={{
-              display: "flex",
-              alignItems: "flex-start",
-            }}
+      <div className="news-app">
+        <h1>News Portal</h1>
+        <div className="category-selector">
+          <span>Category:</span>
+          <select
+            value={selectedCategory}
+            onChange={(e) => this.handleCategoryChange(e.target.value)}
           >
-            Alamat:
-            <textarea
-              cols="30"
-              rows="10"
-              name="alamat"
-              onChange={(e) =>
-                this.setState({ alamat: e.target.value }, () =>
-                  console.log(this.state)
-                )
-              }
-            />
-          </label>
-          <br />
-          <label>
-            Member:{" "}
-            <input
-              type="checkbox"
-              checked={this.state.member}
-              name="member"
-              onChange={(e) =>
-                this.setState({ member: e.target.checked }, () =>
-                  console.log(this.state)
-                )
-              }
-            />
-          </label>
-          <br />
-          <label>
-            <button className="submit-button" type="submit">
-              Submit
-            </button>
-          </label>
-        </form>
+            <option value="all">All</option>
+            <option value="business">Business</option>
+            <option value="technology">Technology</option>
+            <option value="health">Health</option>
+          </select>
+        </div>
+        <input
+          type="text"
+          placeholder="Search News"
+          value={searchTerm}
+          onChange={this.handleSearchChange}
+        />
+        {limitedNews.length > 0 ? (
+          limitedNews.map((article, index) => (
+            <div
+              key={index}
+              className="news-item"
+              onClick={() => this.handleArticleSelect(article)}
+            >
+              <h2>{article.title}</h2>
+              <p>{article.description}</p>
+            </div>
+          ))
+        ) : (
+          <div className="no-news-message">No news available.</div>
+        )}
+        {selectedArticle && (
+          <div className="news-detail">
+            <h2>{selectedArticle.title}</h2>
+            <p>{selectedArticle.description}</p>
+          </div>
+        )}
       </div>
     );
   }
 }
+
+export default NewsApp;
